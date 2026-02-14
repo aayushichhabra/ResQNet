8403dc6ddd36f098188426d4f54cbcc0efb70ec5
smarya-narang <smarya.narang01@gmail.com>
2026-02-14 16:55:13 +0530

    Initial commit with specific folders

--- resqnet-ngo-dashboard/src/main.jsx
+++ resqnet-ngo-dashboard/src/main.jsx
@@ -1,9 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+// We removed <React.StrictMode> to stop the Map from crashing
+ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
-  </React.StrictMode>,
-);
+)
